04689248708b7a19c8c6cd930ca5c1a580008cce
Patrick Alexander Jr <pmalexander5@gmail.com>
2017-09-01 05:54:24 +0000

    adds a jedi name

--- hello_world.py
+++ hello_world.py
@@ -3,11 +3,13 @@ from os import environ
 
 app = Flask(__name__)
 
+#constructs greeting for / and /root pages
 @app.route("/")
 @app.route("/hello")
 def say_hi():
     return "Hello World!"
-    
+
+#constructs a greeting for Patrick with image of kitten
 @app.route("/hello/<name>")
 def hello_person(name):
     html= """
@@ -20,7 +22,12 @@ def hello_person(name):
         <img src="http://i.imgur.com/GvnvT8cb.jpg">
     """
     return html.format(name.title())
-    
+
+#takes first 3 letters of last name and first 2 letters of first name to combine into a Jedi name
+@app.route("/jedi/<first>/<last>")
+def jedi_name(first, last):
+    return last[:3]+first[:2]
+
 if __name__ == "__main__":
     app.run(host=environ['IP'],
         port=int(environ['PORT']))
